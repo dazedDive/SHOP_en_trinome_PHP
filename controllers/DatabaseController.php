5883591393100411f3882c107ec9f222c07c25a1
dazedDive <dazed_dive@hotmail.fr>
2022-10-20 13:20:15 +0200

    ok

--- controllers/DatabaseController.php
+++ controllers/DatabaseController.php
@@ -6,7 +6,9 @@ class DatabaseController{
         $this->action=null;
         $route=$request[0];
         $id=$request[1];
-        
+        if(isset($id) && !ctype_digit($id)){ 
+            return $this;  
+          }
 
     }
 
