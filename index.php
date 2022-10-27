3626da4809b4b59890f668538399aa70041309eb
David Bécu <david.macnimo@outloock.fr>
2022-10-27 13:52:10 +0200

    +index6412

--- index.php
+++ index.php
@@ -22,44 +22,47 @@ $_ENV['env'] = $env;
 // test
 // $init = new Initializer;
 // $init->writeSchemasFiles($tables, true);
+
+
 $request = HttpRequest::instance();
 
 if (
     $_ENV['env'] == 'dev' && !empty($request->route) && $request->route[0] ==
     'init'
-) {
-    if (Initializer::start($request)) {
-        HttpResponse::send(["message" => "Api Initialized"]);
+    ) {
+        if (Initializer::start($request)) {
+            HttpResponse::send(["message" => "Api Initialized"]);
+        }
+        HttpResponse::send(["message" => "Api Not Initialized, try again ..."]);
     }
-    HttpResponse::send(["message" => "Api Not Initialized, try again ..."]);
-}
-//Standard routes
-
-
-// use Controllers\DatabaseController;
-if (!empty($request->route)) {
-    $const = strtoupper($request->route[0]);
-    $key = "Schemas\Table::$const";
-    if (!defined($key)) {
+    
+    // test
+    $init = new Initializer;
+    $init->writeTableFile(true);
+    
+    //Standard routes
+    
+    // use Controllers\DatabaseController;
+    if (!empty($request->route)) {
+        $const = strtoupper($request->route[0]);
+        $key = "Schemas\Table::$const";
+        if (!defined($key)) {
+            HttpResponse::exit(404);
+        }
+    } else {
         HttpResponse::exit(404);
     }
-} else {
-    HttpResponse::exit(404);
-}
-$controller = new DatabaseController($request);
-$result = $controller->execute();
-if ($result) {
-    HttpResponse::send(["data" => $result], 200);
-}
-
-// test
-$init = new Initializer;
-$init->writeTableFile(true);
-
-$tables = DatabaseService::getTables();
-if (empty($request->route) || !in_array($request->route[0], $tables)) {
-    HttpResponse::exit();
-}
-$controller = new DatabaseController($request);
-$result = $controller->execute();
-HttpResponse::send(["data" => $result]);
+    $controller = new DatabaseController($request);
+    $result = $controller->execute();
+    if ($result) {
+        HttpResponse::send(["data" => $result], 200);
+    }
+    
+    
+    $tables = DatabaseService::getTables();
+    if (empty($request->route) || !in_array($request->route[0], $tables)) {
+        HttpResponse::exit();
+    }
+    $controller = new DatabaseController($request);
+    $result = $controller->execute();
+    HttpResponse::send(["data" => $result]);
